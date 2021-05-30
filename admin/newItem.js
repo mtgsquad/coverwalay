072cd36c9760b3b27c8e9f87bc5e59bc1f8935c2
mtgsquad <mtgsquad@mtgsquad-dev.com>
2021-05-30 15:02:04 +0500

    Fix some button problems

---
+++ admin/newItem.js
@@ -0,0 +1,7 @@
+const itemName = document.getElementById('itemName');
+const itemPrice = document.getElementById('itemPrice');
+const itemDescription = document.getElementById('itemDescription');
+
+const submit = document.getElementById('submit');
+
+submit.onclick
